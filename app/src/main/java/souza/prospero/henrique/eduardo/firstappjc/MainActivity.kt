a4c6d062444c326a5c9930b4a3aafeb680791848
Eduardo Henrique <duvrdx@gmail.com>
2024-10-09 18:20:26 -0300

    feat: adicionando features para enviar email usando intent

--- app/src/main/java/souza/prospero/henrique/eduardo/firstappjc/MainActivity.kt
+++ app/src/main/java/souza/prospero/henrique/eduardo/firstappjc/MainActivity.kt
@@ -1,29 +1,32 @@
 package souza.prospero.henrique.eduardo.firstappjc
 
 import android.content.ActivityNotFoundException
+import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import souza.prospero.henrique.eduardo.firstappjc.ui.theme.FirstAppJCTheme
@@ -31,7 +34,6 @@ import souza.prospero.henrique.eduardo.firstappjc.ui.theme.FirstAppJCTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             FirstAppJCTheme {
                 SendMail()
@@ -40,57 +42,93 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+data class EmailData(
+    var email: String = "",
+    var subject: String = "",
+    var body: String = ""
+)
+
 @Composable
 fun SendMail() {
-    var email by remember { mutableStateOf("") }
-    var subject by remember { mutableStateOf("") }
-    var body by remember { mutableStateOf("") }
+    val configuration = LocalConfiguration.current
+    val isPortrait = configuration.orientation == Configuration.ORIENTATION_PORTRAIT
+
+    val emailData by remember { mutableStateOf(EmailData()) }
     val context = LocalContext.current
 
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize().padding(all = 16.dp)
-    ) {
-        Text("Send Mail using Gmail")
-        Column(verticalArrangement = Arrangement.Top) {
-            TextField(
-                value = email,
-                onValueChange = { email = it },
-                label = { Text("Email") },
-                modifier = Modifier.padding(8.dp)
-            )
-            TextField(
-                value = subject,
-                onValueChange = { subject = it },
-                label = { Text("Subject") },
-                modifier = Modifier.padding(8.dp)
-            )
-            TextField(
-                value = body,
-                onValueChange = { body = it },
-                label = { Text("Body") },
-                modifier = Modifier.padding(8.dp)
-            )
-        }
-
-        Button(
-            onClick = {
-                // Criação do Intent ao clicar no botão
-                val sendMailIntent: Intent = Intent(Intent.ACTION_SENDTO).apply {
-                    data = Uri.parse("mailto:$email") // Apenas para email
-                    putExtra(Intent.EXTRA_SUBJECT, subject)
-                    putExtra(Intent.EXTRA_TEXT, body)
-                }
-
-                try {
-                    context.startActivity(Intent.createChooser(sendMailIntent, "Send email to $email"))
-                } catch (ex: ActivityNotFoundException) {
-                    Toast.makeText(context, "No email clients installed.", Toast.LENGTH_SHORT).show()
-                }
-            }
+    if (isPortrait) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top,
+            modifier = Modifier.padding(all = 16.dp).fillMaxSize()
         ) {
-            Text("Send")
+            Text(stringResource(id = R.string.title), modifier = Modifier.padding(bottom = 16.dp))
+            InputFields(emailData, { emailData.email = it }, { emailData.subject = it }, { emailData.body = it })
+            SendButton(emailData, context)
         }
+    } else {
+        Row(modifier = Modifier.padding(all = 16.dp).fillMaxSize()) {
+            Column(modifier = Modifier.weight(3f).padding(end = 8.dp)) {
+                Text(stringResource(id = R.string.title), modifier = Modifier.padding(bottom = 16.dp))
+                InputFields(emailData, { emailData.email = it }, { emailData.subject = it }, { emailData.body = it })
+            }
+            Column(modifier = Modifier.weight(1f).padding(start = 8.dp).align(Alignment.CenterVertically),
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                SendButton(emailData, context)
+            }
+        }
+    }
+}
+
+@Composable
+fun InputFields(
+    emailData: EmailData,
+    onEmailChange: (String) -> Unit,
+    onSubjectChange: (String) -> Unit,
+    onBodyChange: (String) -> Unit
+) {
+    TextField(
+        value = emailData.email,
+        onValueChange = onEmailChange,
+        label = { Text(stringResource(id = R.string.email_label)) },
+        modifier = Modifier.padding(bottom = 8.dp).fillMaxWidth()
+    )
+    TextField(
+        value = emailData.subject,
+        onValueChange = onSubjectChange,
+        label = { Text(stringResource(id = R.string.subject_label)) },
+        modifier = Modifier.padding(bottom = 8.dp).fillMaxWidth()
+    )
+    TextField(
+        value = emailData.body,
+        onValueChange = onBodyChange,
+        label = { Text(stringResource(id = R.string.body_label)) },
+        modifier = Modifier.padding(bottom = 8.dp).fillMaxWidth()
+    )
+}
+
+@Composable
+fun SendButton(emailData: EmailData, context: Context) {
+    Button(
+        onClick = { sendEmail(emailData, context) },
+        modifier = Modifier.padding(top = 16.dp)
+    ) {
+        Text(stringResource(id = R.string.send_mail_button))
+    }
+}
+
+private fun sendEmail(emailData: EmailData, context: Context) {
+    val sendMailIntent = Intent(Intent.ACTION_SENDTO).apply {
+        data = Uri.parse("mailto:")
+        putExtra(Intent.EXTRA_EMAIL, arrayOf(emailData.email))
+        putExtra(Intent.EXTRA_SUBJECT, emailData.subject)
+        putExtra(Intent.EXTRA_TEXT, emailData.body)
+    }
+
+    try {
+        context.startActivity(Intent.createChooser(sendMailIntent, "Send email to ${emailData.email}"))
+    } catch (ex: ActivityNotFoundException) {
+        Toast.makeText(context, "No email clients installed.", Toast.LENGTH_SHORT).show()
     }
 }
 
