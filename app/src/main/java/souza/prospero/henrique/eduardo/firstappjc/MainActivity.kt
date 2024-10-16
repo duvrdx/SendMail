8c86b9555a7ba9a0b7fc4dfde784ade6bf77888b
Eduardo <duvrdx@gmail.com>
2024-10-16 08:41:00 -0300

    Finalizado

--- app/src/main/java/souza/prospero/henrique/eduardo/firstappjc/MainActivity.kt
+++ app/src/main/java/souza/prospero/henrique/eduardo/firstappjc/MainActivity.kt
@@ -12,8 +12,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -22,6 +25,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
@@ -53,80 +57,114 @@ fun SendMail() {
     val configuration = LocalConfiguration.current
     val isPortrait = configuration.orientation == Configuration.ORIENTATION_PORTRAIT
 
-    val emailData by remember { mutableStateOf(EmailData()) }
+    var email by remember { mutableStateOf("") }
+    var subject by remember { mutableStateOf("") }
+    var body by remember { mutableStateOf("") }
+
     val context = LocalContext.current
+
 
     if (isPortrait) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Top,
-            modifier = Modifier.padding(all = 16.dp).fillMaxSize()
+            modifier = Modifier
+                .padding(all = 16.dp)
+                .fillMaxSize()
         ) {
-            Text(stringResource(id = R.string.title), modifier = Modifier.padding(bottom = 16.dp))
-            InputFields(emailData, { emailData.email = it }, { emailData.subject = it }, { emailData.body = it })
-            SendButton(emailData, context)
+            TextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text(stringResource(id = R.string.email_label)) },
+                modifier = Modifier
+                    .padding(bottom = 8.dp)
+                    .fillMaxWidth()
+            )
+            TextField(
+                value = subject,
+                onValueChange = { subject = it },
+                label = { Text(stringResource(id = R.string.subject_label)) },
+                modifier = Modifier
+                    .padding(bottom = 8.dp)
+                    .fillMaxWidth()
+            )
+            TextField(
+                value = body,
+                onValueChange = { body = it },
+                label = { Text(stringResource(id = R.string.body_label)) },
+                modifier = Modifier
+                    .padding(bottom = 8.dp)
+                    .fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.fillMaxHeight())
+            SendButton(email, subject, body, context)
         }
     } else {
-        Row(modifier = Modifier.padding(all = 16.dp).fillMaxSize()) {
-            Column(modifier = Modifier.weight(3f).padding(end = 8.dp)) {
+        Row(modifier = Modifier
+            .padding(all = 16.dp)
+            .fillMaxSize()) {
+            Column(modifier = Modifier
+                .weight(3f)
+                .padding(end = 8.dp)) {
                 Text(stringResource(id = R.string.title), modifier = Modifier.padding(bottom = 16.dp))
-                InputFields(emailData, { emailData.email = it }, { emailData.subject = it }, { emailData.body = it })
+                TextField(
+                    value = email,
+                    onValueChange = { email = it },
+                    label = { Text(stringResource(id = R.string.email_label)) },
+                    modifier = Modifier
+                        .padding(bottom = 8.dp)
+                        .fillMaxWidth()
+                )
+                TextField(
+                    value = subject,
+                    onValueChange = { subject = it },
+                    label = { Text(stringResource(id = R.string.subject_label)) },
+                    modifier = Modifier
+                        .padding(bottom = 8.dp)
+                        .fillMaxWidth()
+                )
+                TextField(
+                    value = body,
+                    onValueChange = { body = it },
+                    label = { Text(stringResource(id = R.string.body_label)) },
+                    modifier = Modifier
+                        .padding(bottom = 8.dp)
+                        .fillMaxWidth()
+                )
             }
-            Column(modifier = Modifier.weight(1f).padding(start = 8.dp).align(Alignment.CenterVertically),
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically),
                 horizontalAlignment = Alignment.CenterHorizontally) {
-                SendButton(emailData, context)
+
+                SendButton(email, subject, body, context)
             }
         }
     }
 }
 
-@Composable
-fun InputFields(
-    emailData: EmailData,
-    onEmailChange: (String) -> Unit,
-    onSubjectChange: (String) -> Unit,
-    onBodyChange: (String) -> Unit
-) {
-    TextField(
-        value = emailData.email,
-        onValueChange = onEmailChange,
-        label = { Text(stringResource(id = R.string.email_label)) },
-        modifier = Modifier.padding(bottom = 8.dp).fillMaxWidth()
-    )
-    TextField(
-        value = emailData.subject,
-        onValueChange = onSubjectChange,
-        label = { Text(stringResource(id = R.string.subject_label)) },
-        modifier = Modifier.padding(bottom = 8.dp).fillMaxWidth()
-    )
-    TextField(
-        value = emailData.body,
-        onValueChange = onBodyChange,
-        label = { Text(stringResource(id = R.string.body_label)) },
-        modifier = Modifier.padding(bottom = 8.dp).fillMaxWidth()
-    )
-}
 
 @Composable
-fun SendButton(emailData: EmailData, context: Context) {
+fun SendButton(email: String, subject: String, body: String, context: Context) {
     Button(
-        onClick = { sendEmail(emailData, context) },
+        onClick = { sendEmail(email, subject, body, context) },
         modifier = Modifier.padding(top = 16.dp)
     ) {
         Text(stringResource(id = R.string.send_mail_button))
     }
 }
 
-private fun sendEmail(emailData: EmailData, context: Context) {
+private fun sendEmail(email: String, subject: String, body: String, context: Context) {
     val sendMailIntent = Intent(Intent.ACTION_SENDTO).apply {
         data = Uri.parse("mailto:")
-        putExtra(Intent.EXTRA_EMAIL, arrayOf(emailData.email))
-        putExtra(Intent.EXTRA_SUBJECT, emailData.subject)
-        putExtra(Intent.EXTRA_TEXT, emailData.body)
+        putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+        putExtra(Intent.EXTRA_SUBJECT, subject)
+        putExtra(Intent.EXTRA_TEXT, body)
     }
 
     try {
-        context.startActivity(Intent.createChooser(sendMailIntent, "Send email to ${emailData.email}"))
+        context.startActivity(Intent.createChooser(sendMailIntent, "Send email to ${email}"))
     } catch (ex: ActivityNotFoundException) {
         Toast.makeText(context, "No email clients installed.", Toast.LENGTH_SHORT).show()
     }
